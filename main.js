a09079bcb3c676a8a2194e2807e8c28ca2a2d7e6
Hansel Reynoso <omarreynoso09@gmail.com>
2022-03-09 17:25:49 -0500

    added files

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+let heading1 = document.querySelector('h1');
+    heading1.style.fontSize ='48px';
+
+
+ // paragrah 
+let paragrah = document.querySelector("p");
+    paragrah.style.color = "lightblue";
+    paragrah.innerText =
+	"Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+
+let item13 = document.querySelector("#item-13");
+// item13.style.color = "rgba(0, 0, 0, .5)";
+    item13.style.opacity = '0.5';
+
+let item3 = document.querySelector("#item-3");
+    item3.innerText = "I say, Hiiiiiiiiiiiiii!";
+
+let image = document.querySelector('.blank');
+    image.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+    image.style.height ='300px';
+
+let photo = document.querySelector('#photo');
+    photo.style.height ='300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item';
+item16.innerText = "Won't get fool again.";
+
+
+
+
+
+
+
